7304a0a37da15ccc8aa76b045d2ddf0b9d3238a8
Bronley Plumb <bplumb@geodecisions.com>
2014-01-13 18:06:09 -0500

    Added google analytics code to the webpage as well as a favicon

--- master.php
+++ master.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title><?php echo isset($title) ? "Rain Massage $title" : "Rain Massage Therapy"; ?></title>
+        <link rel="icon" type="image/png" href="favicon.ico">
         <link href='http://fonts.googleapis.com/css?family=IM+Fell+English+SC' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Lusitana' rel='stylesheet' type='text/css'>
         <?php echo isset($headContents) ? $headContents : ""; ?>
@@ -28,5 +29,22 @@
         <div class="content stacked">
             <?php echo isset($bodyContents) ? $bodyContents : ""; ?>
         </div>
+        <script>
+            (function(i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function() {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+            ga('create', 'UA-47120707-1', 'rainmassage.org');
+            ga('send', 'pageview');
+
+        </script>
     </body>
 </html>
