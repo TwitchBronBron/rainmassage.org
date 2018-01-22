7c310de5447097936cb59895ff941fdda8dbd18f
Bronley Plumb <bronley@gmail.com>
2018-01-22 12:03:15 -0500

    Enhanced the site to be more mobile-friendly

--- build.js
+++ build.js
@@ -1,18 +1,15 @@
 var glob = require('glob');
 var fs = require('fs');
 var path = require('path');
+var nodeWatch = require('node-watch');
+var commandLineArgs = require('command-line-args');
+var colors = require('colors');
+var livereload = require('livereload');
 
-//get every root-level html page from src
-var pagePaths = glob.sync('src/*.html');
-for (var pagePath of pagePaths) {
-    console.log("Processing " + pagePath);
-    var pageFullPath = path.resolve(pagePath);
 
-    var pageContents = processPage(pageFullPath);
-    //write the page to the root directory
-    var filename = path.basename(pageFullPath);
-    fs.writeFileSync(path.join(__dirname, 'docs', filename), pageContents, { flag: 'w' });
-}
+var options = commandLineArgs([
+    { name: 'watch', alias: 'w', type: Boolean },
+]);
 
 function processPage(pageFullPath) {
     var pageDirPath = path.dirname(pageFullPath);
@@ -31,7 +28,7 @@ function processPage(pageFullPath) {
 
         var importFilePath = path.join(pageDirPath, importFilename);
 
-        console.log('   Importing ' + importFilename);
+        console.log(('   Importing ' + importFilename).cyan);
         //get the import page contents 
         var importPageContents = processPage(importFilePath);
         //insert the whitespace at the beginning of each line
@@ -44,4 +41,39 @@ function processPage(pageFullPath) {
     }
     //write the file contents
     return pageContents;
+}
+
+
+function build() {
+    console.log("Building.".yellow.bold);
+    //get every root-level html page from src
+    var pagePaths = glob.sync('src/*.html');
+    for (var pagePath of pagePaths) {
+        console.log(("Processing " + pagePath).blue);
+        var pageFullPath = path.resolve(pagePath);
+
+        var pageContents = processPage(pageFullPath);
+        //write the page to the root directory
+        var filename = path.basename(pageFullPath);
+        fs.writeFileSync(path.join(__dirname, 'docs', filename), pageContents, { flag: 'w' });
+    }
+    console.log(("Build complete: " + new Date().toISOString() + '.').green.bold);
+}
+var server;
+function watch() {
+    build();
+    console.log("Watching files")
+    nodeWatch('src', { recursive: true }, function (evt, name) {
+        console.log((name + " changed.").yellow);
+        build();
+    });
+
+    server = livereload.createServer();
+    server.watch(__dirname + "/docs");
+}
+
+if (options.watch) {
+    watch();
+} else {
+    build();
 }
